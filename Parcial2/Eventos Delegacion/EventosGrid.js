2a4bd0d293293fcd4d623d6175eb6461d2e87d30
Kevin Lagos <kevinlagos977@gmail.com>
2022-10-13 16:13:09 -0500

    agg

--- Parcial2/Eventos Delegacion/EventosGrid.js
+++
@@ -1,8 +0,0 @@
-    
-    var elem=document.getElementById("cont");
-       elem.addEventListener("click",function(evento){
-           console.log(evento.target.id);
-           if(document.getElementById(evento.target.id).classList.contains("caja")){
-               document.getElementById(evento.target.id).classList.toggle("cajaAmarilla");
-            }
-       });
