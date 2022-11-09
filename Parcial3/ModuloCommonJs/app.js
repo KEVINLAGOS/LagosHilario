a750ae29c41e9984c4ae36f223cccf4d9944bfd3
Kevin Lagos <kevinlagos977@gmail.com>
2022-11-08 18:30:28 -0600

    agg

--- Parcial3/ModuloCommonJs/app.js
+++ Parcial3/ModuloCommonJs/app.js
@@ -1,6 +1,6 @@
 const modulo  = require ("./modulo.js");
 console.log(modulo.cuadrado(5));
-var cowsayjs = require("cowsayjs");
 
+var cowsayjs = require("cowsayjs");
 console.log(cowsayjs.moo("can you see me?"));
 
